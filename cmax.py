1f3fcd85b52b93ab15893f6cb5b1e123985b20c9
gabriel00mainka <gab.mainka@gmail.com>
2021-04-22 13:50:49 +0200

    ostatecznie

--- cmax.py
+++ cmax.py
@@ -1,19 +1,5 @@
 import numpy as np
 
-# def CMAX(pi, n, m):     # dla dwóch maszyn
-#     C = np.zeros((len(pi),m),int)
-#     C[0,0] = pi[0].czasy[0]
-#     C[0,1] = C[0,0]+pi[0].czasy[1]
-
-#     for i in range(1,len(pi)):
-#         C[i,0] = C[i-1,0] + pi[i].czasy[0]
-
-#     for i in range(1,len(pi)):
-#         C[i,1] = max(C[i-1,1], C[i,0]) + pi[i].czasy[1]
-
-#     return C, int(C[len(pi)-1,m-1])
-
-
 def CMAX(pi, n, m):     # dla x maszyn
     n = len(pi)         # len() - dlugosc
     m = len(pi[0].czasy)
@@ -22,7 +8,7 @@ def CMAX(pi, n, m):     # dla x maszyn
     C[0,0] = pi[0].czasy[0]
 
     for i in range(1,m):
-        C[0,i] = C[i-1,0] + pi[0].czasy[i]      # pierwszy wiersz macierzy
+        C[0,i] = C[0,i-1] + pi[0].czasy[i]      # pierwszy wiersz macierzy
     
     for i in range(1,n):
         C[i,0] = C[i-1,0] + pi[i].czasy[0]      # pierwsza kolumna macierzy
